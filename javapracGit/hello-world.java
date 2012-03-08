56bd012651543c40fc8486fcf6a32b0bfd8ea249
Ray Bao <baoruihan@gmail.com>
2012-03-08 13:15:51 +0900

    testing

---
+++ javapracGit/hello-world.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class hello_world{
+    public static void main (String[] args)
+    {
+        System.out.println("This is my first testing on java");
+        }
+    
+    
+    
+    }
