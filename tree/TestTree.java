aa2129ba423026c89340c76f685279b718f19577
Ray Bao <baoruihan@gmail.com>
2012-03-16 18:39:15 +0900

    personal

---
+++ tree/TestTree.java
@@ -0,0 +1,46 @@
+public class TestTree{
+    node root;
+    
+    insert(int idata)
+    {
+        private node newnode;
+            if(root == null)
+            {
+                    node = newnode;
+                
+                }
+         else
+         {
+            node current= root;
+            node parent;
+            lef
+            while(current ! =null)
+            {
+                parent = current;
+               if(key>current.idata)
+               {
+                   
+                   current = current.rightchild;
+                   }
+                else
+                {
+                    current = current.leftchild;
+                    
+                    }
+
+                }
+                parent.
+        
+        }
+        }
+    
+    
+    }
+
+
+public class node {
+    node leftchild;
+    node rightchild;
+    int idata;
+    
+    }
