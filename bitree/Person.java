74a486d3e80bc8d9e5fddedd9ce75a8ef71f9073
Rayjim <baoruihan@gmail.com>
2012-03-17 14:24:40 +0900

    from air

---
+++ bitree/Person.java
@@ -0,0 +1,23 @@
+public class Person{
+
+	Person leftChild;
+	Person rightChild;
+	private int key
+	private String lastName;
+	private String firstName;
+	private int ages;
+	Person(int keyinput, String last, String first, int age)
+
+	{
+	      this.key = keyinput;
+	      this.lastName = last;
+	      this.firstName = first;
+	      this.ages = age;
+	}
+	public void display()
+	{
+		System.out.println("ID: "+key+"  Last name: "+lastName+"  FirstName: "+firstName+"  age: "+age);
+	
+	}
+
+}
---
+++ bitree/Personel.java
@@ -0,0 +1,26 @@
+class Personel{
+
+	private Person peoRoot;
+	int nNum;
+        int id;
+	
+        Personel()
+	{
+		personTree =null;
+		nNum = 0;
+		id = 1;
+	
+	}
+	public boolean insert(String lastName, String firstName, int age)
+ 	{
+	 	if (peoRoot==null)
+		peoRoot(id, lastName, firstName, age);
+		else
+		{
+			if()
+		
+		}
+	
+	}
+
+}
